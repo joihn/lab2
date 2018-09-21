f66564e2c4aff8217cb27d1a1f005dd74551e65f
Maxime G <joihn@users.noreply.github.com>
2018-09-21 12:40:13 +0200

    code good for jan

--- lab2_part1/src/main/Main.java
+++ lab2_part1/src/main/Main.java
@@ -42,7 +42,7 @@ public class Main {
                 System.out.println("--------------------------------------------");
                 System.out.println("Help:");
                 System.out.println("--------------------------------------------");
-                System.out.println("-a: Maze with alternatives (defauoooooolt)");
+                System.out.println("-a: Maze with alternatives (defaut)");
                 System.out.println("-n: Maze without alternatives");        // this is master
                 System.out.println("-s: Simple maze (Only walls)");
                 System.out.println("-o: Office like maze");
